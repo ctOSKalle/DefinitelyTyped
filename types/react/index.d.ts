b2fbcc268440760f3c8bc466aa833e768786dd56
eps1lon <silbermann.sebastian@gmail.com>
2023-04-15 10:01:59 +0200

    [react] Ignore statics from element type checking
    would require a lot of work to fix the issues in the consuming libraries
    (e.g. rc-easyui, moxy__next-router-scroll etc)
    .propTypes assignablity is ultimately not important here.

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3163,7 +3163,18 @@ type ReactManagedAttributes<C, P> = C extends { propTypes: infer T; defaultProps
 
 declare global {
     namespace JSX {
-        type ElementType = React.ElementType;
+        // We don't just alias React.ElementType because React.ElementType
+        // historically does more than we need it to.
+        // E.g. it also contains .propTypes and so TS also verifies the declared
+        // props type does match the declared .propTypes.
+        // But if libraries declared their .propTypes but not props type,
+        // or they mismatch, you won't be able to use the class component
+        // as a JSX.ElementType.
+        // We could fix this everywhere but we're ultimately not interested in
+        // .propTypes assignability so we might as well drop it entirely here to
+        //  reduce the work of the type-checker.
+        // TODO: Check impact of making React.ElementType<P = any> = React.JSXElementConstructor<P>
+        type ElementType = string | React.JSXElementConstructor<any>;
         interface Element extends React.ReactElement<any, any> { }
         interface ElementClass extends React.Component<any> {
             render(): React.ReactNode;
